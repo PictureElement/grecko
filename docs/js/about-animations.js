d6e83091092e5db73f5afcaa8da7e9794a1c152d
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-08-18 15:16:04 +0300

    Update docs

--- docs/js/about-animations.js
+++ docs/js/about-animations.js
@@ -1,16 +1,12 @@
 /* About page */
 var title1 = document.querySelector('#title-1');
 var title2 = document.querySelector('#title-2');
-var historyTitle = document.querySelector('#history-title');
 var historyDescription = document.querySelector('#history-description');
-var jobTitle = document.querySelector('#job-title');
 var jobDescription = document.querySelector('#job-description');
 var newsletterForm = document.querySelector('#newsletter-form');
 title1.style.opacity = "0";
 title2.style.opacity = "0";
-historyTitle.style.opacity = "0";
 historyDescription.style.opacity = "0";
-jobTitle.style.opacity = "0";
 jobDescription.style.opacity = "0";
 newsletterForm.style.opacity = "0";
 
@@ -38,26 +34,10 @@ function callback(entries, observer) {
           observer.unobserve(entry.target);
         }
         break;
-      case "history-title":
-        if (entry.intersectionRatio > 0) {
-          historyTitle.style.opacity = "1";
-          historyTitle.className += " animated slideInLeft";
-          // Stop observing target
-          observer.unobserve(entry.target);
-        }
-        break;
       case "history-description":
         if (entry.intersectionRatio > 0) {
           historyDescription.style.opacity = "1";
           historyDescription.className += " animated slideInRight";
-          // Stop observing target
-          observer.unobserve(entry.target);
-        }
-        break;
-      case "job-title":
-        if (entry.intersectionRatio > 0) {
-          jobTitle.style.opacity = "1";
-          jobTitle.className += " animated slideInLeft";
           // Stop observing target
           observer.unobserve(entry.target);
         }
@@ -88,8 +68,6 @@ var observer = new IntersectionObserver(callback, options);
 // Start observing 
 observer.observe(title1);
 observer.observe(title2);
-observer.observe(historyTitle);
 observer.observe(historyDescription);
-observer.observe(jobTitle);
 observer.observe(jobDescription);
 observer.observe(newsletterForm);
