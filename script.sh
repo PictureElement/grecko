8c9ae93fdf796fd28e273e0b7ec5d37e65c16562
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-08-22 21:30:27 +0300

    Modify script.sh to reflect changes.

--- script.sh
+++ script.sh
@@ -14,8 +14,8 @@ grunt uglify
 
 grunt copy
 
-perl -0pi -e 's/    <!-- Bootstrap -->\n    <link rel="stylesheet" href="..\/css\/bootstrap.css">\n    <!-- Web icons -->\n    <link rel="stylesheet" href="..\/css\/font-awesome.css">\n    <!-- Animate.css -->\n    <link rel="stylesheet" href="..\/css\/animate.css">\n    <!-- Default stylesheet -->\n    <link rel="stylesheet" href="..\/css\/main.css">/    <link rel="stylesheet" href="..\/css\/purestyles.css">/' dist/pages/*.html  
-perl -0pi -e 's/    <!-- Bootstrap -->\n    <link rel="stylesheet" href="css\/bootstrap.css">\n    <!-- Web icons -->\n    <link rel="stylesheet" href="css\/font-awesome.css">\n    <!-- Animate.css -->\n    <link rel="stylesheet" href="css\/animate.css">\n    <!-- Default stylesheet -->\n    <link rel="stylesheet" href="css\/main.css">/    <link rel="stylesheet" href="css\/purestyles.css">/' dist/index.html  
+perl -0pi -e 's/    <!-- Bootstrap -->\n    <link rel="stylesheet" href="..\/css\/bootstrap.css">\n    <!-- Animate.css -->\n    <link rel="stylesheet" href="..\/css\/animate.css">\n    <!-- Default stylesheet -->\n    <link rel="stylesheet" href="..\/css\/main.css">/    <link rel="stylesheet" href="..\/css\/purestyles.css">/' dist/pages/*.html  
+perl -0pi -e 's/    <!-- Bootstrap -->\n    <link rel="stylesheet" href="css\/bootstrap.css">\n    <!-- Animate.css -->\n    <link rel="stylesheet" href="css\/animate.css">\n    <!-- Default stylesheet -->\n    <link rel="stylesheet" href="css\/main.css">/    <link rel="stylesheet" href="css\/purestyles.css">/' dist/index.html  
  
 grunt critical
 
