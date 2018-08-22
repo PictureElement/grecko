6a203c5639a24975fbd875a2e2bcb463e17cdbfa
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-08-22 19:13:34 +0300

    Minify assets, Remove unused CSS, Inline critical CSS and create dist/ directory using Grunt. Add .gitignore file to exclude node_modules/.

---
+++ Gruntfile.js
@@ -0,0 +1,123 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    
+    purifycss: {
+      options: {},
+      target: {
+        src: ['src/index.html', 'src/pages/*.html', 'src/js/*.js'],
+        css: ['src/css/*.css'],
+        dest: 'dist/css/purestyles.css'
+      },
+    },
+
+    cssmin: {
+      t1: {
+        files: [{
+          expand: true,
+          cwd: 'dist/css/',
+          src: ['purestyles.css'],
+          dest: 'dist/css/',
+          ext: '.css'
+        }]
+      }
+    },
+    
+    uglify: {
+      t1: {
+        files: [{
+          expand: true,
+          cwd: 'src/js/',
+          src: '*.js',
+          dest: 'dist/js'
+        }]
+      }
+    },
+
+    copy: {
+      t1: {
+        expand: true,
+        cwd: 'src/pages/',
+        src: '*.html',
+        dest: 'dist/pages/'
+      },
+      t2: {
+        expand: true,
+        cwd: 'src/',
+        src: 'index.html',
+        dest: 'dist/'
+      }
+    },
+
+    critical: {
+      t1: {
+        options: {
+          base: './',
+          css: [
+            'dist/css/purestyles.css'
+          ],
+          width: 1920,
+          height: 1080
+        },
+        src: 'dist/index.html',
+        dest: 'dist/index.html'
+      },
+      t2: {
+        options: {
+          base: './',
+          css: [
+            'dist/css/purestyles.css'
+          ],
+          width: 1920,
+          height: 1080
+        },
+        src: 'dist/pages/*.html',
+        dest: './'
+      }
+    },
+
+    htmlmin: {                                    
+      t1: {                                     
+        options: {                                 
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'dist/pages/',
+          src: '*.html',
+          dest: 'dist/pages/'
+        }]
+      },
+      t2: {                                     
+        options: {                                 
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'dist/',
+          src: 'index.html',
+          dest: 'dist/'
+        }]
+      }
+    }
+
+  });
+
+  // Load the grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-purifycss');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-critical');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  
+  // Default task(s).
+  //grunt.registerTask('default', ['clean']);
+  //grunt.registerTask('default', ['purifycss']);
+  //grunt.registerTask('default', ['cssmin']);
+  //grunt.registerTask('default', ['critical']);
+  //grunt.registerTask('default', ['htmlmin']);
+};
---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rm -r dist
+mkdir dist
+
+cp -r src/fonts dist/
+cp -r src/media dist/
+
+grunt purifycss
+
+grunt cssmin
+
+grunt uglify
+
+grunt copy
+
+perl -0pi -e 's/    <!-- Bootstrap -->\n    <link rel="stylesheet" href="..\/css\/bootstrap.css">\n    <!-- Web icons -->\n    <link rel="stylesheet" href="..\/css\/font-awesome.css">\n    <!-- Animate.css -->\n    <link rel="stylesheet" href="..\/css\/animate.css">\n    <!-- Default stylesheet -->\n    <link rel="stylesheet" href="..\/css\/main.css">/    <link rel="stylesheet" href="..\/css\/purestyles.css">/' dist/pages/*.html  
+perl -0pi -e 's/    <!-- Bootstrap -->\n    <link rel="stylesheet" href="css\/bootstrap.css">\n    <!-- Web icons -->\n    <link rel="stylesheet" href="css\/font-awesome.css">\n    <!-- Animate.css -->\n    <link rel="stylesheet" href="css\/animate.css">\n    <!-- Default stylesheet -->\n    <link rel="stylesheet" href="css\/main.css">/    <link rel="stylesheet" href="css\/purestyles.css">/' dist/index.html  
+ 
+grunt critical
+
+grunt htmlmin
