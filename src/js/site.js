1eef0d4b4c1ab514f48d7e297d0b3e029b1d7db8
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-08-24 06:03:30 +0300

    Add back to top button. Improve all social icons. Add smooth scrolling.

---
+++ src/js/site.js
@@ -0,0 +1,27 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'start'
+    });
+  });
+});
+
+// Back to top hide/show
+var $backToTop = $('#back-to-top');
+
+var offset = 2000;
+var duration = 400;
+
+$(window).scroll(function() {
+  if($(this).scrollTop() > offset) {
+    $backToTop.fadeIn(duration);
+  }
+  else {
+    $backToTop.fadeOut(duration);
+  }
+});
+
